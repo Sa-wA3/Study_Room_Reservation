1689f1bc04c085c08a892af389c126b7d1498a16
Takayuki Hirasawa <t-hirasawa@atware.co.jp>
2021-05-27 15:06:47 +0900

    add reservation.db

--- StudyRoomReservation.java
+++ StudyRoomReservation.java
@@ -1,5 +1,10 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class StudyRoomReservation {
 
@@ -13,15 +18,64 @@ public class StudyRoomReservation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StudyRoom sr = new StudyRoom();
+        String dbname = "reservation.db"; // 利用するデータベースファイル
+        Connection conn = null; 
+        Statement stmt = null;
         // Student A = new Student("平澤", 123456, 1);
         // Student B = new Student("吉井", 234567, 2);
         // Student C = new Student("西川", 345678, 2);
         // Student D = new Student("中山", 456789, 1);
-        int seat_num = 0;;
+        int seat_num = 0;
         int doReserve;
         String student_name;
         int student_grade;
 
+        try {
+            Class.forName("org.sqlite.JDBC"); 
+            conn = DriverManager.getConnection("jdbc:sqlite:" + dbname);
+            System.out.println("Connection to " + dbname + " is succeeded.");
+            stmt = conn.createStatement();
+            // stmt.executeUpdate("CREATE TABLE reservation (student_id INTEGER NOT NULL, name VARCHAR(20) NOT NULL, grade INTEGER NOT NULL, PRIMARY KEY (student_id))");
+            // System.out.println("Create reservation table.");
+
+            // stmt.executeUpdate("INSERT INTO reservation VALUES(1, 'Sawa', 1)");
+            // stmt.executeUpdate("INSERT INTO reservation VALUES(2, 'Ken', 2)");
+            // stmt.executeUpdate("INSERT INTO reservation VALUES(3, 'Kazu', 1)");
+            // stmt.executeUpdate("INSERT INTO reservation VALUES(4, 'Kei', 2)");
+            // System.out.println("Completed data insertation.");
+
+            ResultSet rs = stmt
+                    .executeQuery("SELECT * FROM reservation WHERE student_id = 1");
+            // System.out.println("選択");
+            System.out.println("Show reservation table.");
+            while (rs.next()) {
+                int student_id = rs.getInt("student_id");
+                String name = rs.getString("name");
+                int grade = rs.getInt("grade");
+                // System.out.println(student_id + "\t" + name + "\t" + price);
+                System.out.println("会員番号" + student_id + "、" + grade + "年の" + name + "さんですね。");
+                System.out.println("今日も勉強頑張っていきましょう！");
+            }
+            rs.close();
+
+            // stmt.executeUpdate("DROP TABLE products");
+            // System.out.println("テーブル削除");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        
         System.out.println("氏名と学年を入力してください");
         student_name = sc.next();
         student_grade = sc.nextInt();
